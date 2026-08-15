08515ba93b40617fbcbc2eeffda62e5e67f4eec9
Denniz <gabrieldenniz@gmail.com>
2026-08-15 09:07:28 +0800

    added line parser

--- pipeline/main.cpp
+++ pipeline/main.cpp
@@ -17,4 +17,17 @@ struct post
 static std::vector<std::string>parseCV(const std::string& line)
 {
   std::vector<std::string>fields;
+  std::string field;
+  bool inQoutes=false;
+  for(size_t i=0;i<line.size();++i)
+  {
+    char c=line[i];
+    if(c=='"'){inQoutes=!inQoutes;}
+    else if(c==','&&!inQoutes)
+    {
+      fields.push_back(field);
+      field.clear();
+    }
+    else field+=c;
+  }
 }
